b3f8136241a534ae28b6d53b12b5565ec282a052
gregoire lemoulant <gregoire.lemoulant@gmail.com>
2025-12-24 17:06:58 +0100

    Initial landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
+  base: "/bubbleappstudio-site/",
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
